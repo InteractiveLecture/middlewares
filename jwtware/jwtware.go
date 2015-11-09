6590bac421208cab5293400ab2da7c771ff17d33
Rene Richter <Richterrettich@gmail.com>
2015-11-09 19:12:43 +0100

    Initial commit

---
+++ jwtware/jwtware.go
@@ -0,0 +1,35 @@
+package jwtware
+
+import (
+	"github.com/InteractiveLecture/serviceclient"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"io/ioutil"
+	"net/http"
+)
+
+var secret = make([]byte, 0)
+
+func secretHandler(token *jwt.Token) (interface{}, error) {
+	if len(secret) == 0 {
+		serviceInstance := serviceclient.GetInstance("authentication-service")
+		resp, err := serviceInstance.Get("/oauth/public_key")
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		body, decodeErr := ioutil.ReadAll(resp.Body)
+		if decodeErr != nil {
+			return nil, err
+		}
+		secret = body
+	}
+	return secret, nil
+}
+
+func JwtMiddleware(next http.Handler) http.Handler {
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: secretHandler,
+	})
+	return jwtMiddleware.Handler(next)
+}
